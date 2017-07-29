5476e06a854c1acd7f0e9b03a5e241e514e00dfa
Jason De Lorme <jjdelorme@yahoo.com>
2017-07-29 08:08:04 -0400

    Adding upload to azure with command line arguments fixed.

--- chart/azure-upload.js
+++ chart/azure-upload.js
@@ -1,21 +1,60 @@
-var fs = require('fs');
-var azure = require('azure-storage');
-
-// var accountName = "irt8413";
-// var accessKey = "0yDq7ZYiSWueD7bfVQVKk7qEr8NWIUb2tYNgJ7Q5l4PxAY8ZZ1ubof1hxOFZ23jtyQ3cYSBaS9H0zw5ecjyzpw==";
-var connectionString = "DefaultEndpointsProtocol=https;AccountName=irt8413;AccountKey=0yDq7ZYiSWueD7bfVQVKk7qEr8NWIUb2tYNgJ7Q5l4PxAY8ZZ1ubof1hxOFZ23jtyQ3cYSBaS9H0zw5ecjyzpw==;EndpointSuffix=core.windows.net";
-var blobService = azure.createBlobService(connectionString);
-
-blobService.createBlockBlobFromLocalFile('vhds', 'output3.json', '../build/output3.json', function(error, result, response) {
-  if (!error) {
-    // file uploaded
-    console.log(result);
-  }
-});
-
 /*
-// http://azure.github.io/azure-sdk-for-node/azure-storage-legacy/latest/blob_blobservice.js.html
-var blobService = new BlobService(storageAccountOrConnectionStringopt, storageAccessKeyopt, hostopt, authenticationProvideropt)
+ * Copyright (c) 2017 Inside Ride Technologies, LLC. All Rights Reserved.
+ * Author: Jason De Lorme (jason@insideride.com)
+ * Requires: Node.js v4.x or greater.
+ *
+ * Simple test harness to upload file to Azure blob storage.
+ */
 
-fs.createReadStream('task1-upload.txt').pipe(blobService.createBlob('taskcontainer', 'task1', storage.Constants.BlobConstants.BlobTypes.BLOCK));
-*/
+require('process')
+const fs = require('fs');
+const path = require('path');
+const azure = require('azure-storage');
+const commandLineArgs = require('command-line-args');
+
+var cli = commandLineArgs([
+    { name: 'file', alias: 'f', description: 'Path of local file to upload.' },
+    { name: 'output', alias: 'o', type: String, defaultValue: '', description: 'Specify a different destination filename.' }
+]);
+
+//
+// More details on how to use nodejs Azure SDK here: // https://azure.github.io/azure-storage-node/
+//
+// If connection string not specified, it will look for environment variables.
+//  
+const connectionString = "DefaultEndpointsProtocol=https;AccountName=irt8413;AccountKey=0yDq7ZYiSWueD7bfVQVKk7qEr8NWIUb2tYNgJ7Q5l4PxAY8ZZ1ubof1hxOFZ23jtyQ3cYSBaS9H0zw5ecjyzpw==;EndpointSuffix=core.windows.net";
+
+function upload(container, destinationFileName, sourceFilename) {
+  var blobService = azure.createBlobService(connectionString);
+
+  blobService.createBlockBlobFromLocalFile(container, destinationFileName, sourceFilename, 
+    function(error, result, response) {
+      if (!error) {
+        // file uploaded
+        console.log(result);
+      }
+  });
+}
+
+
+function main() {
+    console.log("Uploading...", cli);
+
+    // Required inputs:
+    var container = 'vhds';
+    var sourceFilename = cli.file; //'../build/output3.json';
+    var destinationFileName = cli.output //'output3.json'; 
+
+    if (sourceFilename == null) {
+        //console.log(cli.getUsage());
+        console.log("ERROR: please provide filename.");
+        return;
+    } else if (destinationFileName === '') {
+        // If no destination, just strip path and use the same filename.
+        destinationFileName = path.basename(sourceFilename);
+    }
+
+    upload(container, destinationFileName, sourceFilename);
+}
+
+main();
