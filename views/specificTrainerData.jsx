060c6adcfb3352fc6dc795f583aa19080436b90e
Jason De Lorme <jjdelorme@yahoo.com>
2018-01-02 21:00:16 -0500

    Changes to support other specificTrainerData fields.

--- views/specificTrainerData.jsx
+++ views/specificTrainerData.jsx
@@ -2,6 +2,47 @@
 
 import React from 'react';
 import RideDataComponent from '../views/rideDataComponent.jsx';
+import TargetPowerStatus from '../views/targetPowerStatus.jsx';
+
+function FeState(props) {
+    // Formats the fitness equipment state field into a user string.
+    function formatFeState(feState) {
+        var value = "";
+        switch (feState) {
+            case 1: /*FE_ASLEEP_OFF*/
+                value = "Off";
+                break;
+            case 2: /*FE_READY*/
+                value = "Ready";
+                break;
+            case 3: /*FE_IN_USE*/
+                value = "In use"
+                break;
+            case 4: /*FE_FINISHED_PAUSED*/
+                value = "Finished or Paused.";
+                break;
+            default:
+                value = "Not set.";
+                break;
+        }
+        return value;
+    }
+
+    return (
+        <div className="feState">{formatFeState(props.value)}</div>
+    );
+}
+
+function TrainerStatus(props) {
+    // Does nothing right now...
+    // var status = {
+    //         powerCalibrationRequired : (bits & 0x1),
+    //         resistanceCalibrationRequired : (bits & 0x2),
+    //         userConfigRequired : (bits & 0x4)
+    //     };
+    //     return status;
+    return (<div />);
+}
 
 export default class SpecificTrainerData extends React.Component {
     constructor(props) {
@@ -28,15 +69,20 @@ export default class SpecificTrainerData extends React.Component {
         this.setState({
             instantPower: data.instantPower,
             trainerStatus: data.trainerStatus,
-            target_power_status: data.target_power_status,
+            targetPowerStatus: data.target_power_status,
             feState: data.feState
         });
     }
 
     render() {
         return (
-            <RideDataComponent class="trainerPower" label="WATTS"
-                value={this.state.instantPower} />
+            <div>
+                <RideDataComponent class="trainerPower" label="WATTS"
+                    value={this.state.instantPower} />
+                <TrainerStatus value={this.state.trainerStatus} />
+                <TargetPowerStatus value={this.state.targetPowerStatus} />
+                <FeState value={this.state.feState} />
+            </div>
         );
     }
 }
---
+++ views/targetPowerStatus.jsx
@@ -0,0 +1,37 @@
+'use babel';
+
+import React from 'react';
+
+export default class TargetPowerStatus extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    formatTargetPowerStatus(status) {
+        var value = "";
+        switch (status) {
+            case 0: /*TARGET_AT_POWER*/
+                value = "On Target";
+                break;
+            case 1: /*TARGET_SPEED_TOO_LOW*/
+                value = "Too Slow";
+                break;
+            case 2: /*TARGET_SPEED_TOO_HIGH*/
+                value = "Too Fast";
+                break;
+            default:
+                value = "Target not set.";
+                break;
+        }
+        return value;
+    }
+
+    render() {
+        return (
+            <div className="targetPowerStatus">
+                {this.formatTargetPowerStatus(this.props.value)}
+            </div>
+        );
+    }
+}
+
