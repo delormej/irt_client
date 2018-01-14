07354a6a09d200273b8ed55137c9164f6067e459
Jason De Lorme (x1) <delormej@hotmail.com>
2018-01-14 11:19:30 -0500

    Adding color based on ftp.

--- views/powerMeter.jsx
+++ views/powerMeter.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import RideDataComponent from '../views/rideDataComponent.jsx';
+import ColorStyle from '../lib/ant/ts/colorScale';
 
 export default class PowerMeter extends React.Component {
     constructor(props) {
@@ -10,7 +11,8 @@ export default class PowerMeter extends React.Component {
       this.onBikePower = this.onBikePower.bind(this);
       this.state = {
           instantPower: 0,
-          instantCadence: 0
+          instantCadence: 0,
+          ftp: 280
       };
     }
 
@@ -34,6 +36,7 @@ export default class PowerMeter extends React.Component {
     render() {
         return (
             <RideDataComponent class="bikePower" label="WATTS"
+                style={ColorStyle.getColorStyle(this.state.instantPower, this.state.ftp)}
                 value={this.state.instantPower} />
           );      
     }
