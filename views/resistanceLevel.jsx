26e8c5f373f225de13e062e1adaff56ec4900894
Jason De Lorme (x1) <delormej@hotmail.com>
2018-01-16 10:49:46 -0500

    working on color gradient

--- views/resistanceLevel.jsx
+++ views/resistanceLevel.jsx
@@ -2,18 +2,12 @@
 
 import React from 'react';
 import GeneralSettings from '../views/generalSettings.jsx';
+import ColorScale from '../lib/ant/ts/colorScale';
 
 export default class ResistanceLevel extends GeneralSettings {
     constructor(props) {
       super(props);
-    }
-
-    getResistanceLevelStyle(resistanceLevel) {
-        let style = {
-            background: `linear-gradient(to right, rgb(0,255,0) , rgb(255,255,0), rgb(255,0,0))`,
-            width: `${resistanceLevel}%`  
-        }      
-        return style;
+      this.state.resistanceLevel = 80;
     }
 
     render() {
@@ -21,8 +15,54 @@ export default class ResistanceLevel extends GeneralSettings {
             <div className="resistanceLevelBox">
                 <div className="dataLabel">RESISTANCE LEVEL</div>
                 <div className="dataValue">{this.state.resistanceLevel + '%'}</div>
-                <div className="resistanceLevel" style={this.getResistanceLevelStyle(this.state.resistanceLevel)}></div>
+                <div className="resistanceLevel" style={this.getResistanceLevelStyle()}></div>
             </div>
         );
+    }
+
+    getResistanceLevelStyle() {
+        let resistanceLevel = this.state.resistanceLevel;
+        let style = {
+            /*background: `linear-gradient(to right, rgb(0,255,0) , rgb(255,255,0), rgb(255,0,0))`,*/
+            background: `linear-gradient(to right, 
+                rgb(${this.getGreenGradient(resistanceLevel)}) , 
+                rgb(${this.getYellowGradient(resistanceLevel)}), 
+                rgb(${this.getRedGradeint(resistanceLevel)}))`,
+            width: `${resistanceLevel}%`  
+        }      
+        return style;
+    }
+
+    getGreenGradient() {
+        let r = 0, g = 0, b = 0;
+        let value = this.state.resistanceLevel;
+        if (value < 33) {
+            let percentOfMax = value / 33;
+            g = Math.floor(255 * percentOfMax);
+        }
+        else {
+            g = 255;
+        }
+        return [r,g,b];
+    }
+
+    getYellowGradient() {
+        let r = 0, g = 0, b = 0;
+        let value = this.state.resistanceLevel;
+        if (value < 33) {
+            r = 255;
+            g = 255;
+            b = 255;
+        }
+        else if (value < 66) {
+
+        }
+
+        return [r,g,b];
+    }
+
+    getRedGradeint() {
+        let r = 0, g = 0, b = 0;
+        return [r,g,b];
     }
 }
