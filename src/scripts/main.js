21e39d5fec431ce357d4e851f4bb80ca8b6cc7bd
Jason De Lorme <jjdelorme@yahoo.com>
2018-03-15 19:51:52 -0400

    WIP on azure upload.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from '../views/main';
 import ErrorBoundry from '../views/errorBoundry.jsx';
+
 const remote = require('electron').remote;
 
 window.onload = function(){
@@ -10,6 +11,56 @@ window.onload = function(){
 
   document.getElementById("closeBtn").addEventListener("click", function (e) {
       var window = remote.getCurrentWindow();
-      window.close();
+      //window.close();
+      upload();
   }); 
+}
+
+function upload() {
+  // Docs on how to use Azure node sdk here: https://azure.github.io/azure-storage-node/
+  var azure = require('azure-storage');
+  // This is the blog that I used to figure out how to create a SAS: https://buildazure.com/2017/05/23/azure-cli-2-0-generate-sas-token-for-blob-in-azure-storage/
+  // Note that blog lists how to create SAS for a specific blob (File), switch to "container":
+  // az storage container generate-sas --account-name irt8413 --account-key xxx --name rides --permissions w --expiry 2020-01-01
+    var sas = "se=2020-01-01&sp=w&sv=2017-07-29&sr=c&sig=/czaS/1tXZbewLOjC27a5VIapkOaO4iqbxkiZ0B/kLU%3D";
+  var url = "https://irt8413.blob.core.windows.net/";
+  var containerName = "rides";
+  var uploadFileName = getLogFilename();
+  var localFileName =  getLocalFilename();
+  var connectionString = "BlobEndpoint=" + url + ";SharedAccessSignature=" + sas;
+  var blobService = azure.createBlobService(connectionString);
+
+  blobService.createBlockBlobFromLocalFile(containerName, uploadFileName, localFileName, 
+    function(error, result, response)  
+    {
+      if (error)
+        console.log(error.message);
+      console.log('File uploaded:',  result);
+    }
+  );
+}
+
+function getLogFilename() {
+  var name = "Device-" + new Date().toISOString().replace(/:|\.|-/g,'') + '.txt';
+}
+
+function getLocalFilename() {
+  var fs = require('fs');
+  var lastStat = null;
+
+  var files = fs.readdirSync("Device*.txt");
+  files.forEach(function (file) {
+    var stat = fs.statSync(file);
+    if (lastStat == null) {
+      lastStat = stat;
+    }
+    else {
+      if (stat.atime > lastStat.atime)
+        stat = lastStat;
+    }
+  });
+
+  if (lastStat != null)
+    return lastStat.name;
+  return "";
 }
